136039b1f3f09d4121105d4a10c3ab8a33638bf1
João Vitor Seiji Sato Calvaro <joaovssc@gmail.com>
2024-08-31 10:00:44 -0300

    Main View

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,21 +1,26 @@
 package application;
 	
+import java.io.IOException;
+
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));// Chama o load e carrga a view
+			Parent parent = loader.load();
+
+			Scene mainScene = new Scene(parent);//Cena principal
+			primaryStage.setScene(mainScene);//Palco da cena
+			primaryStage.setTitle("Sample JavaFX application");//Titulo
+			primaryStage.show();//E mostro o palco
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
