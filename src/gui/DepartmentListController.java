d2214495fd8dc0e9637a60e1b6bb0753d865c2eb
João Vitor Seiji Sato Calvaro <joaovssc@gmail.com>
2024-09-12 22:39:30 -0300

    Observer design pattern to update tableview

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import gui.listeners.DataChangeListener;
 import gui.util.Alerts;
 import gui.util.Utils;
 import javafx.collections.FXCollections;
@@ -26,7 +27,7 @@ import javafx.stage.Stage;
 import model.entities.Department;
 import model.services.DepartmentService;
 
-public class DepartmentListController implements Initializable{
+public class DepartmentListController implements Initializable, DataChangeListener{
 
 	
 	private DepartmentService service;//Não possu instanciar aqui, seria um acoplamento forte
@@ -88,6 +89,7 @@ public class DepartmentListController implements Initializable{
 			DepartmentFormController dfc = loader.getController();
 			dfc.setDepartment(obj);
 			dfc.setDepartmentService(new DepartmentService());
+			dfc.subscribeDataChangeListener(this);//Estou me escrevendo para receber o evento e fazer algo, ser notificado é atualizar os dados
 			dfc.updateFormData();
 			
 			Stage dialogStage = new Stage();
@@ -103,5 +105,9 @@ public class DepartmentListController implements Initializable{
 		}catch(IOException e) {
 			Alerts.showAlert("IOException", "Error loading view", e.getMessage(), AlertType.ERROR);
 		}
+	}
+	@Override
+	public void onDataChanged() {
+		updateTableView();
 	}
 }
---
+++ src/gui/listeners/DataChangeListener.java
@@ -0,0 +1,5 @@
+package gui.listeners;
+
+public interface DataChangeListener {
+	void onDataChanged(); //evento a ser disparado quando houver uma mudança
+}
