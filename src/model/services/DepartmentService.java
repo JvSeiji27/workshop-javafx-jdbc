7b15f11631a0a349e0334412380850d3fd0a46f4
João Vitor Seiji Sato Calvaro <joaovssc@gmail.com>
2024-09-26 18:55:35 -0300

    Remove Department

--- src/model/services/DepartmentService.java
+++ src/model/services/DepartmentService.java
@@ -23,5 +23,9 @@ public class DepartmentService {
 			dao.update(obj);
 		}
 	}
+	
+	public void remove(Department obj) { //Remover um departamento do banco de dados
+		dao.deleteById(obj.getId());
+	}
 
 }
